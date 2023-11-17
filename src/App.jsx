117c9ba437b329af2ad98e71f624b13e6c56b88d
sdocouto <mail@sauloduarte.com>
2023-11-16 22:04:49 -0300

    Components Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+import Content from './Components/Content/Content'
+import './App.scss';
 
 const App = () => {
 
   return (
-    <div>
-      app
-    </div>
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
   )
 }
 
