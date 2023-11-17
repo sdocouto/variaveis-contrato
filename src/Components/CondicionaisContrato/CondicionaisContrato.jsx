36d028501d6562b976c06c6240d564b21c48c194
sdocouto <mail@sauloduarte.com>
2023-11-17 16:21:28 -0300

    Condicionais de Contrato page content

--- src/Components/CondicionaisContrato/CondicionaisContrato.jsx
+++ src/Components/CondicionaisContrato/CondicionaisContrato.jsx
@@ -1,8 +1,115 @@
 import React from 'react'
+import './CondicionaisContrato.scss'
 
 const CondicionaisContrato = () => {
   return (
-    <div>CondicionaisContrato</div>
+    <div className="container m-t-40">
+        <div className="bg-conteudo">
+            <h2 className="titulo-page">Aprenda a Utilizar as Variáveis e Criar um Contrato do Zero:</h2>
+
+            <h3>Variáveis Condicionais, de Pessoas e de Valores</h3>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/AIUxW-kQVDQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            <h3>Variáveis Extra, de Assinatura e de Repetição</h3>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/U43uYsytDU0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            <h3>Criação de um Contrato do Zero</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/M4uCXGUnSN8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            <h3 className="m-t-30">1 - Adicionando um Contrato</h3>
+
+            <p>Ao acessar o sistema no módulo de contratos, podemos selecionar o botão de "Criar novo contrato" para adicionar um novo documento ao sistema.</p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/229B49C10755768D216BEF68BA748B66" alt="" />
+
+            <p>Em seguida podemos começar o cadastro do contrato preenchendo os campos solicitados. </p>
+            
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/C8AAB3F82901803DF825FEC9435DDBCE" alt="" />
+            
+            <p>Após isso, basta colar o texto do contrato na caixa de texto. </p>
+
+            <h3 className="m-t-30">2 - Adicionando Variáveis </h3>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/_3LP41GPA7A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            
+            <p>Para adicionar variáveis no contrato, basta buscar por elas nos quadros abaixo do campo de texto. Após encontrar a variável desejada basta selecionar, copiar e colar dentro do texto do contrato. </p>
+            
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/2D0A3D363FED7C0D3F4524BA97F5E192" alt="" />
+
+            <h3 className="m-t-30">3 - Adicionando Imagens </h3>
+
+            <p>Para adicionar uma imagem precisamos antes de tudo <b>SALVAR</b> nosso contrato. Somente depois de salvo que podemos nos dirigir até a aba para adicionar imagens.</p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/7255F5A96C26948B848B7ED7D16324BE" alt="" />
+
+            <p>Clicando no botão "SALVAR" seremos redirecionados a página dos contratos para seguirmos com o próximo passo. </p>
+
+            <p> Na página principal busque o contrato desejado e clique em opções.</p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/6E13696BBC6B78BB24296FF9F0F1BA37" alt="" />
+
+            <p>Clique em Imagens </p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/D35A66730BC093AC3A54F8486000DA57" alt="" />
+
+            <p>Escolha o arquivo e salve. </p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/C87CF401FB9E4FA5872B7451D55E38B5" alt="" />
+
+            <p>Após isso, copie a variável informada da imagem e cole no local desejado no contrato. Basta ir em "editar" que irá redirecionar de volta ao contrato para fazer a colagem.</p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/7D579AB35500E27C898E932F862F269F" alt="" />
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/9F538A7DABE675E539A9F87DC4669A36" alt="" />
+
+            <h3 className="m-t-30">4 - Adicionando uma Variável Extra </h3>
+
+            <p>O processo para adicionar uma variável extra segue a mesma linha das imagens, <b>Opções>Variáveis>Criar nova Variável</b> e preencher as informações solicitadas. </p>
+            
+            <ul>
+                <li>Nome: Será o nome de identificação da variável. Ex: Nome do Filho.</li>
+                <li>Variável: Será a representação da variável. Ex: NOME_FILHO </li>
+                <li>Obrigatório: Usa-se para dizer ao sistema se essa informação é obrigatória ser preenchida. </li>
+                <li>Tipo: Informa-se o tipo de preenchimento da variável. Ex: Número ou Texto. </li>
+                <li>Ativo no Painel: Define se essa variável estará Ativa ou Inativa para uso. </li>
+            </ul>
+
+            <p>Após definir todas os campos basta clicar em salvar. </p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/1FD813A255E656AADC966239FA08316A" alt="" />
+
+            <p>Pronto, para utilizar essa variável basta entrar em "Editar" e nas caixas de variáveis buscar em Campos adicionais, copiar e colar no contrato. </p>
+
+            <p>Digite o nome dos assuntos para esse grupo e clique em "Adicionar". (É necessário que o assunto tenha sido previamente cadastrado em "Assuntos dos Atendimentos", como mostrado nos procedimentos anteriores)</p>
+
+            <img src="https://s3.amazonaws.com/movidesk-files/9698365FCA83E6ED8F8564DBD8FCA2BB" alt="" />
+
+            <p className="boas-vendas m-t-30 text-center">Boas Vendas!</p>
+
+        </div>
+    </div>
   )
 }
 
